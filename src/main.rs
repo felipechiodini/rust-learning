5b157dfe0c8de540a21049f17a832f08f88b7ac0
Felipe Bona <felipechiodinibona@hotmail.com>
2026-05-14 13:15:36 -0300

    save

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Digite algo:");
+
+    // 1. Cria uma String vazia e mutável para armazenar o texto
+    let mut linha = String::new();
+
+    // 2. Lê a entrada do terminal e grava na variável 'linha'
+    io::stdin()
+        .read_line(&mut linha)
+        .expect("Falha ao ler a linha");
+
+    // 3. Remove a quebra de página (\n) automática do final e exibe
+    println!("Você digitou: {}", linha.trim());
 }
